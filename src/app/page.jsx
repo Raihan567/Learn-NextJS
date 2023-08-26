9b153bebb7f5e52eb0d2cdfc1a45435f6fa88edb
Raihan567 <aburaihansarker567@gmail.com>
2023-08-26 08:48:59 +0600

    hero section completed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,24 @@
+import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from "public/hero.png";
 export default function Home() {
   return (
     <div>
-      <h1 className={styles.container}>Home</h1>
+      <h1 className={styles.container}>
+        <div className={styles.item}>
+          <h1 className={styles.title}>
+            Better design for your digital products
+          </h1>
+          <p className={styles.desc}>
+            Turing your idea into Reality. We bring together the teams from the
+            global tech industry
+          </p>
+          <button className={styles.btn}>See Our Work</button>
+        </div>
+        <div className={styles.item}>
+          <Image className={styles.img} alt="hero-image" src={Hero}></Image>
+        </div>
+      </h1>
     </div>
   );
 }
